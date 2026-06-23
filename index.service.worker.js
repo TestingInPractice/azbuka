f1293f837a7b4c3b21c2e302de52e63068818ae6
Халяпин Владимир <halapinvv@MacBook-Air.local>
2026-06-23 22:04:34 +0300

    deploy: обновление сборки

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782238599|52204688';
+const CACHE_VERSION = '1782241436|9693382';
 /** @type {string} */
 const CACHE_PREFIX = 'Азбука-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
