14d42c115fad850b844d88348a116d529569f843
Халяпин Владимир <halapinvv@MacBook-Air.local>
2026-08-05 23:29:38 +0300

    deploy: fix sound unlock script, arrow glyphs, yogurt image case

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785957885|4725676';
+const CACHE_VERSION = '1785961473|18167022';
 /** @type {string} */
 const CACHE_PREFIX = 'Азбука-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
