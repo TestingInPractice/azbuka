82eecff3d6ac514b3b88df918e431e9cf1ad0124
Халяпин Владимир <halapinvv@MacBook-Air.local>
2026-08-05 18:35:39 +0300

    deploy: 19.5:9 portrait PWA

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785421948|7264607';
+const CACHE_VERSION = '1785944057|7289976';
 /** @type {string} */
 const CACHE_PREFIX = 'Азбука-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
