9e1bd6c51feee17b2a63619a59739282b240b16f
Халяпин Владимир <halapinvv@MacBook-Air.local>
2026-08-08 00:39:46 +0300

    deploy: re-apply Bus.addAt(-1) audio guard, expand aspect (no letterbox bands), back button below iOS status bar

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786135543|3696883';
+const CACHE_VERSION = '1786138433|3869886';
 /** @type {string} */
 const CACHE_PREFIX = 'Азбука-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
