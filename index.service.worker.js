4327519c8d6b6c0337dfa7a6fa173b49b020deec
Халяпин Владимир <halapinvv@MacBook-Air.local>
2026-08-08 12:43:11 +0300

    deploy: stop mic capture after recording (iOS voice mode fix); back buttons enlarged 50%

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786180399|11667029';
+const CACHE_VERSION = '1786182141|3287020';
 /** @type {string} */
 const CACHE_PREFIX = 'Азбука-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
