85a2199d6061374ad94515e2226173e706a215d8
Халяпин Владимир <halapinvv@MacBook-Air.local>
2026-08-09 20:26:38 +0300

    deploy: find-letter word image 365x365, ready title 72, card spacer removed, letter squares centered; SW cache bump

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786291766|2443239';
+const CACHE_VERSION = '1786296047|2573099';
 /** @type {string} */
 const CACHE_PREFIX = 'Азбука-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
