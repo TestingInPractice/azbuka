2ca30ca06dd5b072eff3d16a01abefe0742ff2a0
Халяпин Владимир <halapinvv@MacBook-Air.local>
2026-08-09 18:12:20 +0300

    deploy: find-letter circular nav arrows at screen edges, vertically centered; SW cache bump

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786286352|2437426';
+const CACHE_VERSION = '1786288312|2362269';
 /** @type {string} */
 const CACHE_PREFIX = 'Азбука-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
