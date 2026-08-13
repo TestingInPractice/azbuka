7abc6f28a030ff2410600df29a38a0db5bf33465
Халяпин Владимир <halapinvv@MacBook-Air.local>
2026-08-13 14:53:25 +0300

    deploy: regenerate set4/set5 cards (zebra, lynx, electric train, stream, halva) + new words Чайник/Ёршик (teapot, bottle_brush); SW cache bump

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786304805|244459572';
+const CACHE_VERSION = '1786621813|23363184';
 /** @type {string} */
 const CACHE_PREFIX = 'Азбука-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
