824ce21d2789e1fd63bae825d01e54e1aa7a942d
Халяпин Владимир <halapinvv@MacBook-Air.local>
2026-09-23 18:54:21 +0300

    deploy: fix(audio) web default_playback_type=Stream — первый play() OGG больше не теряется; SW cache bump

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790018398|33634892';
+const CACHE_VERSION = '1790178625|17551830';
 /** @type {string} */
 const CACHE_PREFIX = 'Азбука-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
