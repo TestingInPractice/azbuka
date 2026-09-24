cb8c8fd3146dda8558317dbad5012a55ce63ddd6
Халяпин Владимир <halapinvv@MacBook-Air.local>
2026-09-24 13:49:53 +0300

    deploy: тап-таргеты ≥190px (фаза 2 #18) + полная озвучка Silero TTS (new по умолчанию); SW cache bump

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790197969|4755904';
+const CACHE_VERSION = '1790246882|27251239';
 /** @type {string} */
 const CACHE_PREFIX = 'Азбука-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
